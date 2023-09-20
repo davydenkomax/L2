6321f55183a52dbe789d162110dc0890b79cfb9d
msi <msi@DESKTOP-67PGL6O>
2023-09-20 12:10:29 +0700

    Добавьте файлы проекта.

---
+++ L2/L2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+
+void getCost(int cost) {
+    int rubles = cost / 100;
+    int kopecks = cost % 100;
+    std::cout << rubles << "руб " << kopecks / 10 * 10 << "коп \n";
+}
+
+int getVolume(int a) {
+    return pow(a, 3);
+}
+
+int getSurfaceArea(int a) {
+    return 6 * pow(a, 2);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    std::cout << "Объем куба, если a = 5  " << getVolume(5) << "\n";
+    std::cout << "Площадь поверхности, если a = 5  " << getSurfaceArea(5) << "\n";
+}
+

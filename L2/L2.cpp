df9368a55fd7771e2141595fef406053eee0fffa
davydenkomax <davydenko.3x3x3@gmail.com>
2023-09-22 12:22:10 +0700

    v2

--- L2/L2.cpp
+++ L2/L2.cpp
@@ -1,24 +1,47 @@
 ﻿#include <iostream>
-#include <math.h>
+#include <cmath>
 
-void getCost(int cost) {
-    int rubles = cost / 100;
-    int kopecks = cost % 100;
-    std::cout << rubles << "руб " << kopecks / 10 * 10 << "коп \n";
-}
+using namespace std;
 
+/**
+ * @brief вычисление объема куба
+ * @param a длина ребра
+ * @return объем куба
+*/
 int getVolume(int a) {
     return pow(a, 3);
 }
 
+/**
+ * @brief вычисление площади поверхности куба
+ * @param a длина ребра
+ * @return площадь поверхности куба
+*/
 int getSurfaceArea(int a) {
     return 6 * pow(a, 2);
 }
 
-int main()
-{
+/**
+ * @brief задание номер 2. выводит сумму в нужном формате.
+ * @param cost сумма в копейках
+*/
+void getCost(int cost) {
+    int rubles = cost / 100;
+    int kopecks = cost % 100;
+    cout << rubles << "руб " << kopecks / 10 * 10 << "коп\n";
+}
+
+
+int main() {
     setlocale(LC_ALL, "ru");
-    std::cout << "Объем куба, если a = 5  " << getVolume(5) << "\n";
-    std::cout << "Площадь поверхности, если a = 5  " << getSurfaceArea(5) << "\n";
+    int a;
+    cout << "Введите длину куба: ";
+    cin >> a;
+    cout << "Объем куба = " << getVolume(a) << " , если a = " << a << "\n";
+    cout << "Площадь поверхности = " << getSurfaceArea(a) << " , если a = "<< a << "\n";
+    int cost;
+    cout << "Введите сумму в копейках: ";
+    cin >> cost;
+    getCost(cost);
 }
 
